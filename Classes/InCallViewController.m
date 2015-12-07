eb4eda58a944201a3ba84332e67406d155be5d11
Zack Matthews <zack@zackmatthews.com>
2015-12-07 08:42:52 -0500

    VATRP-1154: Ensure IPCTS is showing up without having to first enter chat mode

--- Classes/InCallViewController.m
+++ Classes/InCallViewController.m
@@ -143,6 +143,8 @@ static UICompositeViewDescription *compositeDescription = nil;
     if(self.tableView){
         [self.tableView reloadData];
     }
+    
+    self.isChatMode = NO;
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
